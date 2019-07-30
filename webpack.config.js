7ee99af6eeb9d1586532efa8139ddf51cbb9a7d7
vieiraigor8787 <igor_vc@hotmail.com>
2019-07-30 17:19:34 -0300

    carregando arq .css - style-loader, css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,20 @@ const webpack = require('webpack')
 
 module.exports = {
     mode: 'development',
-    entry: './src/main.js'
+    entry: './src/main.js',
+    output: {
+        filename: 'main.js',
+        path: __dirname + '/public'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, //critério: loader só esta lendo arquivo .css (regEx)
+                use: [
+                    'style-loader', //adiciona CSS a DOM injetando a tag <style>
+                    'css-loader'//interpreta imports, url..
+                ] 
+            }
+        ]
+    }
 }
